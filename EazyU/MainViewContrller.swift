3e23c5f8d648b246212e245e3515c0a8503e4c91
ZJjohn <zhihaojiang@zhihaos-macbook-pro.local>
2017-05-10 16:34:38 -0400

    Initial Commit

---
+++ EazyU/MainViewContrller.swift
@@ -0,0 +1,43 @@
+//
+//  MainViewContrller.swift
+//  EazyU
+//
+//  Created by Zhihao Jiang on 5/9/17.
+//  Copyright © 2017 ZJ Jiang. All rights reserved.
+//
+
+import UIKit
+import GoogleSignIn
+
+class MainViewContrller: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func OnLogOutButton(_ sender: Any) {
+        print(GIDSignIn.sharedInstance().currentUser.userID)
+        if(GIDSignIn.sharedInstance().currentUser != nil) {
+                GIDSignIn.sharedInstance().signOut()
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ EazyU/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  EazyU
+//
+//  Created by Zhihao Jiang on 5/9/17.
+//  Copyright © 2017 ZJ Jiang. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import GoogleSignIn
+class ViewController: UIViewController, GIDSignInUIDelegate {
+
+    @IBOutlet weak var GoogleSigninButton: UIButton!
+    
+    @IBAction func OnclickGoogle(_ sender: Any) {
+        GIDSignIn.sharedInstance().signIn()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
+        //GIDSignIn.sharedInstance().signIn()
+        
+        // TODO(developer) Configure the sign-in button look/feel
+        // ...
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
+
